a8077e58bb34ebd5f804a770aa6e625492913108
sora <hinataandso@gmail.com>
2023-04-10 21:30:23 +0900

    チュートリアル4終了

---
+++ src/app/Http/Controllers/TaskController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Folder;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    //
+    public function index(int $id)
+    {
+        $folders = Folder::all();
+
+        $current_folder = Folder::find($id);
+
+        $tasks = $current_folder->tasks()->get();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $id,
+            'tasks' => $tasks,
+        ]);
+    }
+}
