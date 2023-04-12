1df431e80927abf880b8a43927a8dd5551b11049
sora <hinataandso@gmail.com>
2023-04-12 22:46:52 +0900

    チャプター7終了

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -35,16 +35,41 @@ class TaskController extends Controller
     public function create(int $id, CreateTask $request)
     {
         $current_folder = Folder::find($id);
-    
+
         $task = new Task();
         $task->title = $request->title;
         $task->due_date = $request->due_date;
-    
+
         $current_folder->tasks()->save($task);
-    
+
         return redirect()->route('tasks.index', [
             'id' => $current_folder->id,
         ]);
     }
 
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
+
+    public function edit(int $id, int $task_id, EditTask $request)
+    {
+        // 1
+        $task = Task::find($task_id);
+
+        // 2
+        $task->title = $request->title;
+        $task->status = $request->status;
+        $task->due_date = $request->due_date;
+        $task->save();
+
+        // 3
+        return redirect()->route('tasks.index', [
+            'id' => $task->folder_id,
+        ]);
+    }
 }
