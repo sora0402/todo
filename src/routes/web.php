ba264327e8aef8da99723b02c73598a859d4a1c4
sora <hinataandso@gmail.com>
2023-04-13 02:09:04 +0900

    チャプター10終了

--- src/routes/web.php
+++ src/routes/web.php
@@ -21,18 +21,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
-Route::group(['middleware' => 'auth'], function() {
-Route::get('/folders/{id}/tasks', [TaskController::class,'index'])->name('tasks.index');
-Route::get('/folders/create', [FolderController::class,'showCreateForm'])->name('folders.create');
-Route::post('/folders/create', [FolderController::class,'create']);
-Route::get('/folders/{id}/tasks/create', [TaskController::class,'showCreateForm'])->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', [TaskController::class,'create']);
-Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class,'showEditForm'])->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class,'edit']);
-Route::get('/', [HomeController::class,'index'])->name('home');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+
+    Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+    Route::post('/folders/create', 'FolderController@create');
+
+    Route::group(['middleware' => 'can:view,folder'], function () {
+        Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
+
+        Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+        Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
+
+        Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+        Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
+    });
 });
 
 Auth::routes();
-
-
